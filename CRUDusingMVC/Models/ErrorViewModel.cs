4db44153212c3d563ce54882aa590efda6b22d87
AnkushKukade <ankushkukade1@gmail.com>
2022-05-31 00:25:02 +0530

    Add project files.

---
+++ CRUDusingMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDusingMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
